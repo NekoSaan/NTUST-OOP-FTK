9118f1545e43caf26406465553254c9af73a4f97
minfengtang <169050538+minfengtang@users.noreply.github.com>
2024-05-08 17:17:58 +0800

    verison_2
    Modify the variable to follow the framework and debug some error

---
+++ src/GameManager.h
@@ -0,0 +1,13 @@
+#pragma once
+#include <vector>
+#include "Rect.h"
+using namespace std;
+
+static int mapHeight = 50;
+static int mapWidth = 140;
+
+static int cameraHeight;
+static int cameraWidth;
+
+static int cameraX = 0;
+static int cameraY = 0;
