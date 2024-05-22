964294440e0b20f65b3c2346bdc300aeb5d2be20
B11215058 <justin940829@gmail.com>
2024-05-22 16:55:51 +0800

    ?

--- src/dice.h
+++ src/dice.h
@@ -1,26 +1,9 @@
 #ifndef _DICE_H_
 #define _DICE_H_
 #include <stdlib.h>
-#define maxProbability 90
+#define MAXPROBABILITY 90
 
-int dice(int focus, int dicePool, int probability) {
-	if (probability > maxProbability) {
-		probability = maxProbability;
-	}
+// The rool dice
+int dice(int focus, int dicePool, int probability);
 
-	if (dicePool <= focus) {
-		return dicePool;
-	}
-
-	int count = 0;
-
-	for (int i = 0; i < dicePool - focus; i++) {
-		if (probability > rand() % 100) {
-			count++;
-		}
-	}
-
-	return count + focus;
-}
-
-#endif // _DICE_H_
+#endif _DICE_H_
